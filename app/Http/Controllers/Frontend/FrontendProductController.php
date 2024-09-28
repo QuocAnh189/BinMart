5bd2a6c1afcead7967fd5e625db4841a958aa72e
QuocAnh189 <anhquoc18092003@gmail.com>
2024-09-28 15:35:56 +0700

    update product detail page

--- app/Http/Controllers/Frontend/FrontendProductController.php
+++ app/Http/Controllers/Frontend/FrontendProductController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductReview;
 use Illuminate\Http\Request;
 
 class FrontendProductController extends Controller
 {
     public function product_list(Request $request) {}
 
-    public function show_product_detail(string $slug) {}
+    public function show_product_detail(string $slug)
+    {
+        $product = Product::with(['vendor', 'category', 'productImageGalleries', 'variants', 'brand'])
+            ->where('slug', $slug)->where('status', 1)
+            ->first();
+        $reviews = ProductReview::where('product_id', $product->id)->where('status', 1)->paginate(10);
+
+        return view('frontend.pages.product-detail', compact('product', 'reviews'));
+    }
 
     public function change_list_view(Request $request) {}
 }
