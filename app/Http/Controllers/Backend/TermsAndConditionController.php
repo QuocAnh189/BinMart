64853462a97db0abc1780d236f0ef49b4ddf8751
QuocAnh189 <anhquoc18092003@gmail.com>
2024-09-30 20:56:16 +0700

    uupdate term-page page

--- app/Http/Controllers/Backend/TermsAndConditionController.php
+++ app/Http/Controllers/Backend/TermsAndConditionController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\TermsAndCondition;
 use Illuminate\Http\Request;
 
 class TermsAndConditionController extends Controller
 {
-    public function index() {}
+    public function index()
+    {
+        $content = TermsAndCondition::first();
+        return view('admin.terms.index', compact('content'));
+    }
 
-    public function update(Request $request) {}
+    public function update(Request $request)
+    {
+        $request->validate([
+            'content' => ['required']
+        ]);
+
+        TermsAndCondition::updateOrCreate(
+            ['id' => 1],
+            [
+                'content' => $request->content
+            ]
+        );
+
+        toastr('updated successfully!', 'success');
+
+        return redirect()->back();
+    }
 }
--- resources/views/admin/terms/index.blade.php
+++ resources/views/admin/terms/index.blade.php
@@ -6,5 +6,28 @@
         <div class="section-header">
             <h1>Terms and conditions</h1>
         </div>
+
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            Terms and Conditions
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('admin.terms-and-conditions.update') }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <div class="form-group">
+                                    <label>Content</label>
+                                    <textarea class="summernote" name="content">{!! @$content->content !!}</textarea>
+                                </div>
+                                <button class="btn btn-primary" type="submmit">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
