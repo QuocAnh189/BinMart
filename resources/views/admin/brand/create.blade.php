56ed23aedc5befc5119049b9951b93c0fc62d4bb
QuocAnh189 <anhquoc18092003@gmail.com>
2024-09-30 15:18:01 +0700

    update brand page

--- resources/views/admin/brand/create.blade.php
+++ resources/views/admin/brand/create.blade.php
@@ -1,10 +1,52 @@
 @extends('admin.layouts.master')
 
 @section('content')
-    <!-- Main Content -->
     <section class="section">
         <div class="section-header">
             <h1>Brand</h1>
+        </div>
+
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Create Brand</h4>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('admin.brand.store') }}" enctype="multipart/form-data" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <label>Logo</label>
+                                    <input class="form-control" name="logo" type="file">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input class="form-control" name="name" type="text" value="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="inputState">Is Featured</label>
+                                    <select class="form-control" id="inputState" name="is_featured">
+                                        <option value="">Select</option>
+                                        <option value="1">Yes</option>
+                                        <option value="0">No</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputState">Status</label>
+                                    <select class="form-control" id="inputState" name="status">
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                                <button class="btn btn-primary" type="submmit">Create</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
--- resources/views/admin/brand/edit.blade.php
+++ resources/views/admin/brand/edit.blade.php
@@ -1,10 +1,58 @@
 @extends('admin.layouts.master')
 
 @section('content')
-    <!-- Main Content -->
     <section class="section">
         <div class="section-header">
             <h1>Brand</h1>
+        </div>
+
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Update Brand</h4>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('admin.brand.update', $brand->id) }}" enctype="multipart/form-data"
+                                method="POST">
+                                @csrf
+                                @method('PUT')
+                                <div class="form-group">
+                                    <label>Preview</label>
+                                    <br>
+                                    <img alt="" src="{{ asset($brand->logo) }}" width="200">
+                                </div>
+                                <div class="form-group">
+                                    <label>Logo</label>
+                                    <input class="form-control" name="logo" type="file">
+                                </div>
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input class="form-control" name="name" type="text" value="{{ $brand->name }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputState">Is Featured</label>
+                                    <select class="form-control" id="inputState" name="is_featured">
+                                        <option value="">Select</option>
+                                        <option {{ $brand->is_featured == 1 ? 'selected' : '' }} value="1">Yes</option>
+                                        <option {{ $brand->is_featured == 0 ? 'selected' : '' }} value="0">No</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputState">Status</label>
+                                    <select class="form-control" id="inputState" name="status">
+                                        <option {{ $brand->status == 1 ? 'selected' : '' }} value="1">Active</option>
+                                        <option {{ $brand->status == 0 ? 'selected' : '' }} value="0">Inactive</option>
+                                    </select>
+                                </div>
+                                <button class="btn btn-primary" type="submmit">Update</button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
