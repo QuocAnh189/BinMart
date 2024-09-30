7e9549ab9364a54c038d9dc25bd5ca783551bd22
QuocAnh189 <anhquoc18092003@gmail.com>
2024-09-30 20:02:43 +0700

    update about page

--- app/Http/Controllers/Backend/AboutController.php
+++ app/Http/Controllers/Backend/AboutController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\About;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    public function index() {}
+    public function index()
+    {
+        $content = About::first();
+        return view('admin.about.index', compact('content'));
+    }
 
-    public function update(Request $request) {}
+    public function update(Request $request)
+    {
+        $request->validate([
+            'content' => ['required']
+        ]);
+
+        About::updateOrCreate(
+            ['id' => 1],
+            [
+                'content' => $request->content
+            ]
+        );
+
+        toastr('updated successfully!', 'success');
+
+        return redirect()->back();
+    }
 }
