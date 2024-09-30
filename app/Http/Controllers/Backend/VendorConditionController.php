7d147aee954baf348523e2e82b0d17120c3498d2
QuocAnh189 <anhquoc18092003@gmail.com>
2024-09-30 19:58:07 +0700

    update vendor-condition page

--- app/Http/Controllers/Backend/VendorConditionController.php
+++ app/Http/Controllers/Backend/VendorConditionController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\VendorCondition;
 use Illuminate\Http\Request;
 
 class VendorConditionController extends Controller
 {
-    public function index() {}
+    public function index()
+    {
+        $content = VendorCondition::first();
+        return view('admin.vendor-condition.index', compact('content'));
+    }
 
-    public function update(Request $request) {}
+    public function update(Request $request)
+    {
+        $request->validate([
+            'content' => ['required']
+        ]);
+
+        VendorCondition::updateOrCreate(
+            ['id' => 1],
+            [
+                'content' => $request->content
+            ]
+        );
+
+        toastr('updated successfully!', 'success');
+
+        return redirect()->back();
+    }
 }
--- resources/views/admin/vendor-condition/index.blade.php
+++ resources/views/admin/vendor-condition/index.blade.php
@@ -1,10 +1,34 @@
 @extends('admin.layouts.master')
 
 @section('content')
-    <!-- Main Content -->
     <section class="section">
         <div class="section-header">
             <h1>Vendor Condition</h1>
+        </div>
+
+        <div class="section-body">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Vendor Condition</h4>
+                        </div>
+
+                        <div class="card-body">
+                            <form action="{{ route('admin.vendor-condition.update') }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <div class="form-group">
+                                    <label>Content</label>
+                                    <textarea class="summernote" name="content">{!! @$content->content !!}</textarea>
+                                </div>
+                                <button class="btn btn-primary" type="submmit">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
