752989a42e41a24bd313394750331b7ef66d1b02
QuocAnh189 <anhquoc18092003@gmail.com>
2024-10-28 12:26:44 +0700

    add subcribers admin

--- app/Http/Controllers/Backend/SubscribersController.php
+++ app/Http/Controllers/Backend/SubscribersController.php
@@ -4,13 +4,37 @@ namespace App\Http\Controllers\Backend;
 
 use App\DataTables\Admin\NewsletterSubscriberDataTable;
 use App\Http\Controllers\Controller;
+use App\Mail\Newsletter;
+use App\Models\NewsletterSubscriber;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class SubscribersController extends Controller
 {
-    public function index(NewsletterSubscriberDataTable $dataTable) {}
+    public function index(NewsletterSubscriberDataTable $dataTable)
+    {
+        return $dataTable->render('admin.subscriber.index');
+    }
 
-    public function send_mail(Request $request) {}
+    public function send_mail(Request $request)
+    {
+        $request->validate([
+            'subject' => ['required'],
+            'message' => ['required']
+        ]);
 
-    public function destroy(string $id) {}
+        $emails = NewsletterSubscriber::where('is_verified', 1)->pluck('email')->toArray();
+
+        Mail::to($emails)->send(new Newsletter($request->subject, $request->message));
+
+        toastr('Mail has been send', 'success');
+
+        return redirect()->back();
+    }
+
+    public function destroy(string $id)
+    {
+        NewsletterSubscriber::findOrFail($id)->delete();
+        return response(['status' => 'success', 'message' => 'deleted successfully']);
+    }
 }
--- resources/views/admin/subscriber/index.blade.php
+++ resources/views/admin/subscriber/index.blade.php
@@ -6,5 +6,51 @@
         <div class="section-header">
             <h1>Subscribers</h1>
         </div>
+
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Send Email to all subscribers</h4>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('admin.subscribers-send-mail') }}" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="">Subject</label>
+                                    <input class="form-control" name="subject" type="text">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Message</label>
+                                    <textarea class="form-control" name="message"></textarea>
+                                </div>
+                                <button class="btn btn-primary" style="submit">Send</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section">
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>All Subscribers</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $dataTable->table() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
+
+@push('scripts')
+    {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+@endpush
